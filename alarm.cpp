c3c1d5b9bcb95acfad4eea381b1f3134a9e6584a
Thomas van Kleef <thomasvankleef@gmail.com>
2018-01-02 17:04:24 +0100

    Added example code for calculating crc8

--- alarm.cpp
+++ alarm.cpp
@@ -14,8 +14,34 @@ alarm::alarm()
 }
 
 void doMij() {
-	DEBUG_STR("Doe mij!\n");
+	uint16_t len = 6;
+	uint8_t data[6] = {0, 2, 3, 4, 5, 255};
+	uint8_t crc, crc8;
+	char str[80];
+
 	PINB = (1<<5);
+
+	crc = 0x00;
+	crc8 = 0x00;
+
+	for (uint16_t i = 0; i < len; i++) {
+		crc ^= data[i];
+
+		for (uint8_t j = 0; j < 8; j++) {
+			if (crc & 0x01) {
+                crc = (crc >> 1) ^ 0xA001;
+			} else {
+				crc = (crc >> 1);
+			}
+			crc |= (crc << i);
+		}
+
+		crc8 ^= crc;
+	}
+	sprintf(str, "crc = %i\n", crc8);
+	DEBUG_STR(str);
+
+	// return crc;	
 }
 
 void alarm::addNewAlarm(datetime_t executeTime, void (*func)(void)) {
